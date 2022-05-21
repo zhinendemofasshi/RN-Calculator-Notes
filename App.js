0daac4ebf82afc479d4c6794adb11f18c08cbfec
Ike Yang <70048185+zhinendemofasshi@users.noreply.github.com>
2022-05-21 14:18:48 +0800

    Update App.js

--- App.js
+++ App.js
@@ -2,29 +2,43 @@ import * as React from 'react';
 import { Text, View} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator } from "@react-navigation/native-stack";
 import NOTE from "./app/components/file";
 import App2 from "./app/components/Calculator";
-function HomeScreen() {
+import Menu from "./app/components/menu";
+function OuterScreen({ navigation }) {
     return (
-        <App2/>
+        <App2
+            navigation = {navigation}
+        />
     );
 }
-
-function SettingsScreen() {
-    return (
-        <NOTE/>
+function DiaryScreen({ route, navigation }){
+    return(
+        <NOTE
+            navigation ={ navigation }
+            route = {route}
+        />
     );
 }
-
+function InnerScreen({ navigation }) {
+    return (
+        <Menu
+            navigation={ navigation }
+        />
+    );
+}
+const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 export default function App() {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name="Home" component={HomeScreen} />
-                <Tab.Screen name="Settings" component={SettingsScreen} />
-            </Tab.Navigator>
+            <Stack.Navigator initialRouteName="Inner">
+                <Stack.Screen name="Outer" component={OuterScreen} />
+                <Stack.Screen name="Inner" component={InnerScreen} />
+                <Stack.Screen name="Diary" component={DiaryScreen} />
+            </Stack.Navigator>
         </NavigationContainer>
     );
 }
