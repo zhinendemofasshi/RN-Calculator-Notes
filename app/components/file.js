b02e52f38dfd5cefaebdfc229f129da1efd26ba9
Kismet777 <69754294+Kismet777@users.noreply.github.com>
2022-05-21 21:38:30 +0800

    Update file.js

--- app/components/file.js
+++ app/components/file.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { 
+import {
     View,
-    Text, 
-    TextInput, 
-    StyleSheet,  
+    Text,
+    TextInput,
+    StyleSheet,
     Alert,
     TouchableOpacity,
 } from 'react-native';
@@ -16,45 +16,57 @@ const Button = (props) => (
         </View>
     </TouchableOpacity>
 )
+var flag1;
 const NOTE = (props) => {
-    const [notes, setnotes] = useState("");
-    console.log(notes);
+    // console.log(notes); for test
 
     const {path} = props.route.params;
+    const  {content} = props.route.params;
+    const [notes, setnotes] = useState(content);
 
-    console.log("path of now:" + path);
-    const _read = (path) => {
+    // setnotes(content);
+
+    // console.log("path of now:" + path); for test
+    const _read = async (path) => {
         RNFS.readFile( path, 'utf8')
             .then(content => {
+                console.log(content);
                 setnotes(content);
+                console.log("notes:" + notes);
             })
     }
-    const _update = (notes, path) => {
+    const _update = async(notes, path) => {
         RNFS.writeFile(path, notes, 'utf8')
             // .then(notes => console.log(notes))
-            .then(() => console.log("update it !!"))
+            .then(() => console.log(path + ": update it !!"))
     }
-    _read(path);
+    // _read(path);
+    // if(flag){
+    //     _read(path);
+    //     flag = 0;
+    // }
     return (
         <View>
             <TextInput
                 style={styles.input}
-                onChangeText={
-                    text => {
-                        setnotes(text);
-                        console.log("TEXT: " + text);
-                        console.log("notes: " + notes);
-                    }
-                }
-                // value={notes}
+                // onChangeText = {
+                //     text => {
+                //         setnotes(text);
+                //         console.log("TEXT: " + text);
+                //         console.log("notes: " + notes);
+                //     }
+                // }
+                value={notes}
+                onChangeText = {text => setnotes(text)}
                 placeholder="Write anything you'd like to note."
                 multiline={true}
             />
             <Button
                 onPress={() => {
-                    // console.log("notes:" + notes);
-                     _update(notes, path);
-                    }}
+                    console.log("notes:" + notes);
+                    _update(notes, path);
+                    // _read(path); decide if the file has been stored
+                }}
                 content="save"
             />
             <Button
@@ -68,20 +80,20 @@ const NOTE = (props) => {
     );
 }
 const styles = StyleSheet.create({
-    BackButton :{
-        height:70,
-        width:90,
-    },
-    ButtonText:{
-        fontSize:50,
-    },
-    input: {
-        height: 200,
-        width: 400,
-        margin: 12,
-        borderWidth: 1,
-        padding: 0,
+        BackButton :{
+            height:70,
+            width:90,
+        },
+        ButtonText:{
+            fontSize:50,
+        },
+        input: {
+            height: 200,
+            width: 400,
+            margin: 12,
+            borderWidth: 1,
+            padding: 0,
+        }
     }
-}
 )
 export default NOTE;
